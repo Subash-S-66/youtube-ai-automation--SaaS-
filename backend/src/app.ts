1f58b32be9df7c25a6b980b4f9dead040c4663cc
Subash-S-66 <subash.93450@gmail.com>
2026-03-21 18:15:11 +0530

    Register schedule routes in backend API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,6 +12,7 @@ import supportRoutes from './routes/supportRoutes';
 import adminRoutes from './routes/adminRoutes';
 import userRoutes from './routes/userRoutes';
 import bannerRoutes from './routes/bannerRoutes';
+import scheduleRoutes from './routes/scheduleRoutes';
 import { errorHandler, AppError } from './middleware/errorHandler';
 import * as Sentry from '@sentry/node';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
@@ -96,6 +97,7 @@ app.use('/api/support', supportRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/banner', bannerRoutes);
+app.use('/api/schedules', scheduleRoutes);
 
 // Base route
 app.get('/', (req: Request, res: Response) => {
